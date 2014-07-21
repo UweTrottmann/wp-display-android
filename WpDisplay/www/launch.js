3661f8b004606dbae13b0e961d0c585efbdcf80e
Uwe Trottmann <uwe.trottmann@gmail.com>
2014-07-21 15:43:22 +0200

    Initial code copy from WP Display v2.3.0.

---
+++ WpDisplay/www/launch.js
@@ -0,0 +1,15 @@
+/**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/apps/app.runtime.html
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('index.html', {
+  	id: "mainwin",
+    bounds: {
+      width: 450,
+      height: 600
+    }
+  });
+});
