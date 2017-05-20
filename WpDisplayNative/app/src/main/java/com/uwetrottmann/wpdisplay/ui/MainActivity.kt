fa908df4461c862840ea3a276d8f627dc8758b0b
Uwe Trottmann <uwe+git@uwetrottmann.com>
2017-05-20 16:59:46 +0200

    Convert MainActivity to Kotlin.

---
+++ WpDisplayNative/app/src/main/java/com/uwetrottmann/wpdisplay/ui/MainActivity.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2015 Uwe Trottmann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.uwetrottmann.wpdisplay.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+import com.uwetrottmann.wpdisplay.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // setup action bar
+        val actionBarToolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(actionBarToolbar)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, DisplayFragment())
+                    .commit()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                supportFragmentManager.popBackStack()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
