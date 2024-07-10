3057350f00294233f951e1e04bb496de73457cf7
Uwe Trottmann <uwe+git@uwetrottmann.com>
2024-07-10 17:41:20 +0200

    Update material [1.7.0 -> 1.12.0]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,7 +91,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
     // Material Design
     // https://github.com/material-components/material-components-android/releases
-    implementation("com.google.android.material:material:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("com.jakewharton.timber:timber:5.0.1")
 
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
