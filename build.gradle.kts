a1befd8861896746006c6fc5c665c08f6c6f7cba
Uwe Trottmann <uwe+git@uwetrottmann.com>
2025-10-07 07:45:17 +0200

    Gradle: extract plugin version to TOML version catalog file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
-    // https://github.com/ben-manes/gradle-versions-plugin/releases
-    id("com.github.ben-manes.versions") version("0.52.0")
-    // https://kotlinlang.org/docs/releases.html#release-details
-    // https://kotlinlang.org/docs/gradle-configure-project.html#apply-the-plugin
-    // Before updating look for compatible coroutines, Gradle and Android plugin version
-    // 2.1.21 is compatible with Gradle 7.6.3–8.12.1 and AGP 7.3.1–8.7.2
-    id("org.jetbrains.kotlin.jvm") version "2.1.21" apply false
-    id("com.android.application") version "8.7.2" apply false
+    alias(libs.plugins.versions)
+    alias(libs.plugins.kotlin) apply false
+    alias(libs.plugins.android) apply false
 }
 
 tasks.wrapper {
