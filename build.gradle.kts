eb2e13f0ab4b2b5e23513342e58f7c1fe7d43c7c
Uwe <uwe@uwetrottmann.com>
2026-09-19 06:49:21 +0200

    Update dependencies to latest versions, remove versions plugin
    Lint can check for available updates now.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
-    alias(libs.plugins.versions)
     alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.android) apply false
 }
 
 tasks.wrapper {
     distributionType = Wrapper.DistributionType.ALL
-}
-
-// reject preview releases for dependencyUpdates task
-fun isNonStable(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
-    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-    val isStable = stableKeyword || regex.matches(version)
-    return isStable.not()
-}
-tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
-    rejectVersionIf {
-        isNonStable(candidate.version)
-    }
 }
