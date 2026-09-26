9ce06937c9226770329d8ce4a2d419a2b30d8a57
Uwe <uwe@uwetrottmann.com>
2026-09-26 09:00:53 +0200

    IntentTools: add tryStartActivity helper

--- app/src/main/java/com/uwetrottmann/wpdisplay/util/IntentTools.kt
+++ app/src/main/java/com/uwetrottmann/wpdisplay/util/IntentTools.kt
@@ -7,16 +7,35 @@ import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import androidx.core.net.toUri
+import timber.log.Timber
 
 fun openWebPage(context: Context, url: String) {
     val webpage = url.toUri()
     val intent = Intent(Intent.ACTION_VIEW, webpage)
+    context.tryStartActivity(intent)
+}
+
+/**
+ * Calls [Context.startActivity] with the given [Intent]. Returns false if
+ * no activity found to handle it.
+ *
+ * This is useful for example for an implicit intent that may fail to open the web browser
+ * app if it is disabled in a restricted profile.
+ */
+fun Context.tryStartActivity(intent: Intent): Boolean {
     // Note: Android docs suggest to use resolveActivity,
     // but won't work on Android 11+ due to package visibility changes.
     // https://developer.android.com/about/versions/11/privacy/package-visibility
+    var handled: Boolean
     try {
-        context.startActivity(intent)
+        startActivity(intent)
+        handled = true
     } catch (e: ActivityNotFoundException) {
-        // Ignored
+        Timber.i(e, "Failed to launch intent.")
+        handled = false
+    } catch (e: SecurityException) {
+        Timber.i(e, "Failed to launch intent.")
+        handled = false
     }
+    return handled
 }
